255418d65971b9dfdcee2bdf083c2eb8340e8f36
Ariel Eli <arieleli@MacBook-Air-sl-Ariel.local>
2026-06-02 21:30:06 +0300

    test(stats): cover size_format and show_internal_database_sizes params

--- spec/meilisearch/client/stats_spec.rb
+++ spec/meilisearch/client/stats_spec.rb
@@ -13,4 +13,20 @@ RSpec.describe 'Meilisearch::Client - Stats' do
     response = client.stats
     expect(response).to have_key('databaseSize')
   end
+
+  it 'gets stats with human-readable size format' do
+    response = client.stats(size_format: 'human')
+    expect(response).to have_key('databaseSize')
+    expect(response['databaseSize']).to be_a(String)
+  end
+
+  it 'gets stats with internal database sizes' do
+    client.create_index('books').await
+    response = client.stats(show_internal_database_sizes: true)
+
+    expect(response).to have_key('indexes')
+    response['indexes'].each_value do |index_stats|
+      expect(index_stats).to have_key('internalDatabaseSizes')
+    end
+  end
 end
--- spec/meilisearch/index/stats_spec.rb
+++ spec/meilisearch/index/stats_spec.rb
@@ -9,6 +9,18 @@ RSpec.describe 'Meilisearch::Index - Stats' do
     expect(response).not_to be_empty
   end
 
+  it 'returns stats with human-readable size format' do
+    response = index.stats(size_format: 'human')
+    expect(response).to have_key('rawDocumentDbSize')
+    expect(response['rawDocumentDbSize']).to be_a(String)
+  end
+
+  it 'returns stats with internal database sizes' do
+    response = index.stats(show_internal_database_sizes: true)
+    expect(response).to have_key('internalDatabaseSizes')
+    expect(response['internalDatabaseSizes']).to be_a(Hash)
+  end
+
   it 'gets the number of documents' do
     response = index.number_of_documents
     expect(response).to eq(documents.count)
